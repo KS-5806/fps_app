e2f373a375ab8b1829b695528fcce51b2c3dab9a
ABC <xxxxx@xxx.com>
2024-05-29 04:01:10 +0900

    動画投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,28 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('posts.create.success', item: Post.model_name.human)
+    else
+      flash.now['danger'] = t('posts.create.failure', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- spec/system/posts/post_spec.rb
+++ spec/system/posts/post_spec.rb
@@ -43,5 +43,39 @@ RSpec.describe '投稿動画', type: :system do
         end
       end
     end
+    describe '動画の作成' do
+      context 'ログインしていない場合' do
+        it 'ログインページにリダイレクトされること' do
+          visit '/posts/new'
+          Capybara.assert_current_path("/login", ignore_query: true)
+          expect(current_path).to eq('/login'), 'ログインしていない場合、動画作成画面にアクセスした際に、ログインページにリダイレクトされていません'
+          expect(page).to have_content('ログインしてください'), 'フラッシュメッセージ「ログインしてください」が表示されていません'
+        end
+      end
+
+      context 'ログインしている場合' do
+        before do
+          login_as(user)
+          click_on('動画作成')
+        end
+
+        it '動画が作成できること' do
+          fill_in 'タイトル', with: 'テストタイトル'
+          fill_in '内容', with: 'テスト内容'
+          click_button '登録'
+          Capybara.assert_current_path("/posts", ignore_query: true)
+          expect(current_path).to eq('/posts'), '動画一覧画面に遷移していません'
+          expect(page).to have_content('動画を作成しました'), 'フラッシュメッセージ「動画を作成しました」が表示されていません'
+          expect(page).to have_content('テストタイトル'), '作成した動画のタイトルが表示されていません'
+          expect(page).to have_content('テスト内容'), '作成した動画の内容が表示されていません'
+        end
+
+        it '動画の作成に失敗すること' do
+          fill_in 'タイトル', with: 'テストタイトル'
+          click_button '登録'
+          expect(page).to have_content('動画を作成出来ませんでした'), 'フラッシュメッセージ「動画を作成出来ませんでした」が表示されていません'
+        end
+      end
+    end
   end
 end
