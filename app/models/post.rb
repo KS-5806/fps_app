035f1f8558d3fe2a6dcbd3fc8061d7fa907fcdbe
ABC <xxxxx@xxx.com>
2024-06-04 23:58:30 +0900

    コメント機能の追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :movie, MovieUploader
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
